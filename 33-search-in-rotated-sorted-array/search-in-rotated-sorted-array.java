21d5e98daf1ab2c13f694089983581853321facd
Sandeep Mopuri <sandeepmopuri387@gmail.com>
2026-04-07 19:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (72.34%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = pivot(nums);
+
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    public int binarySearch(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int pivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1; 
+            }
+
+            if (nums[start] > nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
